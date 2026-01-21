50302ddceb412a189bc82c9e7a95c839f9ff3cfd
Leon Xu <mail4leon@126.com>
2026-01-22 00:05:40 +0800

    shell

---
+++ shell/stop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo -e "\033[34m-------------------wsgi process------------------\033[0m"
+ps -ef | grep jtbd_uwsgi.ini | grep -v grep
+sleep 0.5
+echo -e "\n----------------------going to close-----------------------"
+ps -ef | grep jtbd_uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
+sleep 0.5
