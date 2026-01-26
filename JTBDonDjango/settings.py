d077d8c243a63a47460b4609cf9632dcd3c9dcc1
Leon Xu <mail4leon@126.com>
2026-01-26 15:04:00 +0800

    dns

--- JTBDonDjango/settings.py
+++ JTBDonDjango/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)2kc9vdun3^wtgh(a3lgptk(vczd$p^$+^6_+=3fdh4n0$l+cg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['47.108.118.15']
+ALLOWED_HOSTS = ['agent-flow.cn', 'www.agent-flow.cn', '47.108.118.15']
 
 # Application definition
 
